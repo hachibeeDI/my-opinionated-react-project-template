7c2560889dbf9fae3b6ef2c1b69c917b90a04c66
OguraDaiki <8hachibee125@gmail.com>
2021-01-04 16:57:38 +0900

    setup better typescript-lib

---
+++ src/bootstrap.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+
+/// <reference path="../node_modules/better-typescript-lib/lib.esnext.d.ts" />
+/// <reference path="../node_modules/better-typescript-lib/lib.dom.d.ts" />
